309ea46b5ca8a0c6c472a54ddec2957205e79133
luis personal <lu0211@hotmail.com>
2023-10-26 15:15:46 +0800

    luu

--- pages/index.js
+++ pages/index.js
@@ -260,6 +260,8 @@ export default function Home({ data, allPostsData }) {
         <Link href="/posts/post1">this post</Link>
         <Link href="/posts/post1">this post</Link>
         <Link href="/posts/post1">this post</Link>
+        <Link href="/posts/post1">this post</Link>
+        <Link href="/posts/post1">this post</Link>
         <p>
           Hi! This is Luis from Taiwan. I'm a newbie in programming but willing
           to explore more about front-end development. Feel free to chat or
